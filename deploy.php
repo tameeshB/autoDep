29b18a518e70347e58e0595776e0970d23d133b0
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2017-12-07 01:48:06 +0530

    complete

--- deploy.php
+++ deploy.php
@@ -2,9 +2,11 @@
 /**
  * This script is the landing POST request handler for the github webhook payload.
  */
+ini_set('display_errors', '1');     # don't show any errors....
 
+ echo print_r($_POST['payload']['commits']);
  $_POST = json_decode(file_get_contents('php://input'), true);
 
-
- print_r($_POST);
+var_dump($_POST);
+ echo print_r($_POST);
  ?>
